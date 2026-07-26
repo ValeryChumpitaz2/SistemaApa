fbce75e5fa8b37ab0de27dd8499603ac8473b3a1
Valery Chumpitaz <valerychumpitaz2@gmail.com>
2026-07-25 21:30:21 -0500

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,89 +1,47 @@
 import {
-Routes,
-Route,
-Navigate
+  Routes,
+  Route,
+  Navigate
 } from "react-router-dom";
 
-
+import LandingPage from "../pages/LandingPage";
 import Login from "../pages/Login";
-
 import StudentDashboard from "../modules/student/pages/Dashboard";
-
 import TeacherDashboard from "../modules/teacher/pages/TeacherDashboard";
-
 import ProtectedRoute from "../auth/ProtectedRoute";
 
+export default function AppRoutes() {
+  return (
+    <Routes>
 
+      <Route
+        path="/"
+        element={<LandingPage />}
+      />
 
-export default function AppRoutes(){
+      <Route
+        path="/login"
+        element={<Login />}
+      />
 
+      <Route
+        path="/teacher/dashboard"
+        element={
+          <ProtectedRoute rol="DOCENTE">
+            <TeacherDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-return (
+      <Route
+        path="/student/dashboard"
+        element={
+          <ProtectedRoute rol="ESTUDIANTE">
+            <StudentDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-<Routes>
-
-
-<Route
-
-path="/"
-
-element={
-<Navigate to="/login"/>
-}
-
-/>
-
-
-
-<Route
-
-path="/login"
-
-element={<Login/>}
-
-/>
-
-
-
-<Route
-
-path="/teacher/dashboard"
-
-element={
-
-<ProtectedRoute rol="DOCENTE">
-
-<TeacherDashboard/>
-
-</ProtectedRoute>
-
-}
-
-/>
-
-
-
-<Route
-
-path="/student/dashboard"
-
-element={
-
-<ProtectedRoute rol="ESTUDIANTE">
-
-<StudentDashboard/>
-
-</ProtectedRoute>
-
-}
-
-/>
-
-
-
-</Routes>
-
-);
-
-
+    </Routes>
+  );
 }
